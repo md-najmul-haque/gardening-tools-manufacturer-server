cb9dedb0a45c30ce11d7dd86b7626f70e382cb67
najuml-bd <najmul0612@gmail.com>
2022-05-23 16:16:07 +0600

    tools api created

--- index.js
+++ index.js
@@ -13,13 +13,29 @@ app.use(express.json())
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.uwupg.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-    const collection = client.db("test").collection("devices");
-    // perform actions on the collection object
-    console.log('db connected')
-    client.close();
-});
 
+
+
+
+async function run() {
+    try {
+        await client.connect();
+        const toolsCollection = client.db("gardening_tools").collection("tools");
+
+        app.get('/tools', async (req, res) => {
+            const query = {};
+            const cursor = toolsCollection.find(query);
+            const result = await cursor.toArray();
+            res.send(result);
+
+        })
+    }
+    finally {
+
+    }
+}
+
+run().catch(console.dir)
 
 app.get('/', (req, res) => {
     res.send('Gardening Tools Manufacturer is running')
